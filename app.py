83af7215a7e402b1e46b4d94c79b373a681d5a77
qcarlyroot-arch <qcarlyroot@gmail.com>
2026-04-14 23:13:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 import eventlet
-eventlet.monkey_patch()from flask import Flask, render_template
+eventlet.monkey_patch()
+
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hacker_secret_123'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @socketio.on('message')
-def handle_message(msg):
-    print('Mesaj Alındı: ' + msg)
-    # Mesajı herkese gönder
-    emit('message', msg, broadcast=True)
-
-@socketio.on('image')
-def handle_image(data):
-    # Görsel verisini herkese yayar
-    emit('image', data, broadcast=True)
+def handle_message(data):
+    # Kullanıcı adı ve mesajı alıp herkese gönderiyoruz
+    name = data.get('name', 'Anonim')
+    msg = data.get('msg', '')
+    emit('message', {'name': name, 'msg': msg}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-    
+    socketio.run(app, host='0.0.0.0', port=5000)
