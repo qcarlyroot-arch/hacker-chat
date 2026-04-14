8923cca90fe4b5aa501d0d0adf0d1cc4733f70b7
QCARLY <qcarly@QCARLY.QCARLY>
2026-04-14 21:17:51 +0300

    ilk yukleme

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hacker_secret_123'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Mesaj Alındı: ' + msg)
+    # Mesajı herkese gönder
+    emit('message', msg, broadcast=True)
+
+@socketio.on('image')
+def handle_image(data):
+    # Görsel verisini herkese yayar
+    emit('image', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+    
